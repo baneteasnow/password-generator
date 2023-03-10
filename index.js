2e7dfdf525b30e646c320043c147239356639891
baneteasnow <81329385+baneteasnow@users.noreply.github.com>
2023-03-10 22:28:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
 let passwordLength = 12
 let generateEl = document.getElementById("generate")
 let resetBtn = document.getElementById("resetBtn")
 
 function getRandomCharacter() {
-    let randomChar = Math.floor(Math.random() * characters.length)
-    return characters[randomChar]
+  let randomChar = Math.floor(Math.random() * characters.length)
+  return characters[randomChar]
 }
-
 
 function generateRandomPassword() {
-    let randomPassword = ""
-    for (let i = 0; i < passwordLength; i++) {
-        randomPassword += getRandomCharacter()         
-    }
-    return randomPassword
+  let randomPassword = ""
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword += getRandomCharacter()
+  }
+  return randomPassword
 }
-
-const generatedPasswordOne = generateRandomPassword()
 
 function passWord() {
-     generateEl.textContent = generatedPasswordOne 
+  generateEl.textContent = generateRandomPassword() ///////////
 }
 
-
 resetBtn.addEventListener("click", function() {
-    document.getElementById("generate").innerText = "...";
+  generateEl.innerText = "..."
 });
-  
 
-function copyEvent(id){
-    var str = document.getElementById(id);
-    window.getSelection().selectAllChildren(str);
-    document.execCommand('Copy')
-    }
+function copyEvent(id) {
+  var str = document.getElementById(id);
+  window.getSelection().selectAllChildren(str);
+  document.execCommand('Copy')
+}
+
 function myFunction() {
-        document.getElementById("copied").textContent = "Copied!";
-     }
-
-
+  document.getElementById("copied").textContent = "Copied!";
+}
